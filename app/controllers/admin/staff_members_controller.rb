ea7ffadcb0dc725a400ea12ad69b5e80a2605fe0
mackxxx <yathatatha-6666@yahoo.co.jp>
2020-12-13 23:59:59 +0900

    10.2.3Done

--- app/controllers/admin/staff_members_controller.rb
+++ app/controllers/admin/staff_members_controller.rb
@@ -5,7 +5,7 @@ class Admin::StaffMembersController < Admin::Base
   
   def show
     staff_member = StaffMember.find(params[:id])
-    redirect_to[:edit, :admin, staff_member]
+    redirect_to [ :edit, :admin, staff_member ]
   end
 
   def new
@@ -24,5 +24,23 @@ class Admin::StaffMembersController < Admin::Base
     else 
       render action: "new"
     end
+  end
+
+  def update
+    @staff_member = StaffMember.find(params[:id])
+    @staff_member.assign_attributes(params[:staff_member])
+    if @staff_member.save
+      flash.notice = "職員アカウントを更新しました。"
+      redirect_to :admin_staff_members
+    else
+      render action: "edit"
+    end
+  end
+
+  def destroy
+    staff_member = StaffMember.find(params[:id])
+    staff_member.destroy!
+    flash.notice = "職員アカウントを削除しました。"
+    redirect_to :admin_staff_members
   end
 end
