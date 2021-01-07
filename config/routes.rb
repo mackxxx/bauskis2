aeded64491636a556c594a2e37211d5870a4af96
mackxxx <yathatatha-6666@yahoo.co.jp>
2021-01-08 08:59:59 +0900

    7.3.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
   namespace :staff do
     root "top#index"
+    get "login" => "sessions#new", as::login
+      post"session" => "sessions#create", as::session
+      delete"session" => "sessions#destroy"
   end
 
   namespace :admin do
@@ -11,3 +15,4 @@ Rails.application.routes.draw do
     root "top#index"
   end
 end
+
