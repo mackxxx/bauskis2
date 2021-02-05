013054ef6ab4d8cbae0087611080ab12945855d8
mackxxx <yathatatha-6666@yahoo.co.jp>
2021-02-05 08:59:59 +0000

    Chapter5まで

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   layout :set_layout
   
-  private
-    
+  private 
+  
   def set_layout
     if params[:controller].match(%r{\A(staff|admin|customer)/})
       Regexp.last_match[1]
